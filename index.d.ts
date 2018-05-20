98955a3ed15ad8d48b6695700418fe25e381b4d7
Aleh Kashnikau <aleh.kashnikau@gmail.com>
2018-05-20 14:14:35 +0300

    updated react to version 16

--- index.d.ts
+++ index.d.ts
@@ -4,29 +4,38 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
-/// <reference types="react" />
-
 declare module "@overlai/react-scrollbar" {
+  import React from "react";
   interface ScrollAreaProps extends React.Props<ScrollArea> {
-      className?: string,
-      style?: React.CSSProperties,
-      speed?: number,
-      contentClassName?: string,
-      contentStyle?: React.CSSProperties,
-      vertical?: boolean,
-      verticalContainerStyle?: React.CSSProperties,
-      verticalScrollbarStyle?: React.CSSProperties,
-      horizontal?: boolean,
-      horizontalContainerStyle?: React.CSSProperties,
-      horizontalScrollbarStyle?: React.CSSProperties,
-      onScroll?: (value: {leftPosition: number, topPosition: number, containerHeight: number, containerWidth: number, realHeight: number, realWidth: number}) => void,
-      contentWindow?: any,
-      ownerDocument?: any,
-      smoothScrolling?: boolean
-      minScrollSize?: number,
-      swapWheelAxes?: boolean,
-      focusableTabIndex?: number,
-      stopScrollPropagation?: boolean
+    className?: string;
+    style?: React.CSSProperties;
+    speed?: number;
+    contentClassName?: string;
+    contentStyle?: React.CSSProperties;
+    vertical?: boolean;
+    verticalContainerStyle?: React.CSSProperties;
+    verticalScrollbarStyle?: React.CSSProperties;
+    horizontal?: boolean;
+    horizontalContainerStyle?: React.CSSProperties;
+    horizontalScrollbarStyle?: React.CSSProperties;
+    onScroll?: (
+      value: {
+        leftPosition: number;
+        topPosition: number;
+        containerHeight: number;
+        containerWidth: number;
+        realHeight: number;
+        realWidth: number;
+      }
+    ) => void;
+    contentWindow?: any;
+    ownerDocument?: any;
+    smoothScrolling?: boolean;
+    minScrollSize?: number;
+    swapWheelAxes?: boolean;
+    focusableTabIndex?: number;
+    stopScrollPropagation?: boolean;
+    keyboardScrolling?: boolean;
   }
 
   class ScrollArea extends React.Component<ScrollAreaProps, {}> {}
